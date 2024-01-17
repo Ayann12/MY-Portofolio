391e5b930ab9f5cad21a30564ea40506d0095f2b
Ayann12 <tantowi818@gmail.com>
2024-01-17 15:21:07 +0700

    design baru my porto

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed((".skills"), {
+    strings: ["Frontend Developer", "Designer", "Youtuber", "Network"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+})
